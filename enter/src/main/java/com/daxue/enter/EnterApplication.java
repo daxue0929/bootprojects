31a36090dc7f3d78a5679d9f10e1d198fda6fe1a
wangxuedi <wangxuedi@izton.com>
2019-10-11 18:10:47 +0800

    初始化0

--- enter/src/main/java/com/daxue/enter/EnterApplication.java
+++ enter/src/main/java/com/daxue/enter/EnterApplication.java
@@ -2,12 +2,21 @@ package com.daxue.enter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class EnterApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EnterApplication.class, args);
+    }
+
+    @GetMapping(value = "/")
+    public String test () {
+
+        return "启动成功";
     }
 
 }
