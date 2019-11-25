545df2c6de5b24decc41ea87762f41d678cdf4bc
daxue0929 <ainidehuiji@163.com>
2019-11-26 00:12:24 +0800

    0

--- enter/src/main/java/com/daxue/enter/service/TodayService.java
+++ enter/src/main/java/com/daxue/enter/service/TodayService.java
@@ -12,5 +12,5 @@ import com.baomidou.mybatisplus.extension.service.IService;
  * @since 2019-11-25
  */
 public interface TodayService extends IService<Today> {
-    public void test();
+    void test();
 }
