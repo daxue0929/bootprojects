510ead50885254f46812fd29a95692d2825a41c8
daxue0929 <ainidehuiji@163.com>
2019-12-03 19:29:19 +0800

    合并代码-1

--- enter/src/main/java/com/daxue/enter/controller/GroupController.java
+++ enter/src/main/java/com/daxue/enter/controller/GroupController.java
@@ -1,9 +1,13 @@
 package com.daxue.enter.controller;
 
 
+import com.daxue.enter.entity.Group;
+import com.daxue.enter.service.GroupService;
+import com.daxue.enter.util.ResultData;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
