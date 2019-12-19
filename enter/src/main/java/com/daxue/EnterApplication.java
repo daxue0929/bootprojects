97dc6a0454d686256a3deb74a892bad989f236c8
wangxuedi <wangxuedi@izton.com>
2019-12-19 09:11:51 +0800

    环境切换配置-maven依赖排除

--- enter/src/main/java/com/daxue/EnterApplication.java
+++ enter/src/main/java/com/daxue/EnterApplication.java
@@ -13,3 +13,12 @@ public class EnterApplication {
     }
 }
 
+/**
+ *
+ * 1. YAML 无法使用 @PropertySource 注解加载YAML文件
+ *
+ * 2. 开发项目之前，频繁的在项目环境之间切换，这个时候有大量的配置需要更改
+ *    例如数据库配置，redis配置，mongodb配置，jms配置
+ *
+ *
+ */
