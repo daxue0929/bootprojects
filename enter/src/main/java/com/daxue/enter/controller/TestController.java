5e644e6a00eed008727a4b102355c502c588edc0
wangxuedi <wangxuedi@izton.com>
2019-12-20 17:24:43 +0800

    easypoi4.01读不到文件file not found

--- enter/src/main/java/com/daxue/enter/controller/TestController.java
+++ enter/src/main/java/com/daxue/enter/controller/TestController.java
@@ -1,6 +1,9 @@
 package com.daxue.enter.controller;
 
+import cn.afterturn.easypoi.excel.ExcelExportUtil;
+import cn.afterturn.easypoi.excel.entity.TemplateExportParams;
 import com.daxue.enter.util.ResultData;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ui.Model;
@@ -9,9 +12,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.*;
 
 @CrossOrigin
 @RestController
@@ -43,7 +48,7 @@ public class TestController {
      * @param model
      * @return result
      */
-    @GetMapping(value = "/tes03")
+    @GetMapping(value = "/test03")
     public ResultData test03(@RequestParam Integer id, Model model) {
         ResultData result = new ResultData();
         Map<String, Object> map = model.asMap();
@@ -59,6 +64,45 @@ public class TestController {
         }
 
         result.setData(sb);
+
+        return result;
+    }
+
+    @GetMapping(value = "/test04")
+    public ResultData test04() {
+        ResultData result = new ResultData();
+
+        File file = new File("/static/aa.xlsx");
+        System.out.println(file);
+
+        TemplateExportParams params = new TemplateExportParams("/static/aa.xlsx");
+
+        Map<String, Object> map = new HashMap<String, Object>();
+
+        List<Map<String, String>> names = new ArrayList<Map<String, String>>();
+        HashMap<String, String> itemRow = new HashMap<>();
+        for (int i = 0; i < 3; i++) {
+            itemRow.put("name", "王雪迪");
+
+            names.add(itemRow);
+        }
+        map.put("maplist", names);
+        Workbook workbook = ExcelExportUtil.exportExcel(params, map);
+        File savefile = new File("/static/aa.xlsx");
+        if (!savefile.exists()) {
+            savefile.mkdirs();
+        }
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream("/static/aa.xlsx");
+            workbook.write(fos);
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
         return result;
     }
