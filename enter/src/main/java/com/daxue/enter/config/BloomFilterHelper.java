20f8a4a7a9d23880f01bdf378b6f7d710117cb1f
wangxuedi <wangxuedi@izton.com>
2019-12-26 18:34:13 +0800

    3

---
+++ enter/src/main/java/com/daxue/enter/config/BloomFilterHelper.java
@@ -0,0 +1,49 @@
+package com.daxue.enter.config;
+
+import com.daxue.enter.entity.User;
+import com.daxue.enter.mapper.UserMapper;
+import com.google.common.hash.BloomFilter;
+import com.google.common.hash.Funnel;
+import com.google.common.hash.Funnels;
+import org.apache.poi.ss.formula.functions.T;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+public class BloomFilterHelper<S> {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    private BloomFilter<Integer> bf;
+
+    /**
+     * 创建布隆过滤器
+     *
+     * @PostConstruct：程序启动时候加载此方法
+     */
+    @PostConstruct
+    public void initBloomFilter() {
+        List<User> userList = userMapper.selectList(null);
+        if (CollectionUtils.isEmpty(userList)) {
+            return;
+        }
+        //创建布隆过滤器(默认3%误差)
+        bf = BloomFilter.create(Funnels.integerFunnel(), userList.size());
+        for (User user : userList) {
+            bf.put(user.getId());
+        }
+    }
+
+    /**
+     * 判断id可能存在于布隆过滤器里面
+     *
+     * @param id
+     * @return
+     */
+    public boolean userIdExists(int id) {
+        return bf.mightContain(id);
+    }
+}
