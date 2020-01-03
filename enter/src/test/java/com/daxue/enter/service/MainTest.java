4b0706d9ae6afefd33e16dc6901f71c12a4d3d79
wangxuedi <wangxuedi@izton.com>
2020-01-03 18:37:00 +0800

    周末提交

---
+++ enter/src/test/java/com/daxue/enter/service/MainTest.java
@@ -0,0 +1,32 @@
+package com.daxue.enter.service;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@WebAppConfiguration
+public class MainTest {
+
+
+    @Before
+    public void init() {
+        System.out.println("开始测试--------------");
+    }
+
+
+    @Test
+    public void test() {
+        System.out.println("测试代码");
+    }
+
+    @After
+    public void after() {
+        System.out.println("测试结束--------------");
+    }
+}
