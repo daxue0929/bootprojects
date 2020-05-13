a71094402a750b0d4834ef659153d1ef3c56dae0
wangxuedi <wangxuedi@izton.com>
2020-05-13 19:28:57 +0800

    12467897889

--- test/src/main/java/com/daxue/test/controller/TestController.java
+++ test/src/main/java/com/daxue/test/controller/TestController.java
@@ -1,20 +1,35 @@
 package com.daxue.test.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
 
 /**
  * @author Admin
  * @date 2020/03/12
  **/
 @RestController
+@Slf4j
 public class TestController {
 
-    @GetMapping("")
+    @GetMapping("/tttttt")
     public String test() {
 
-
         return "";
+    }
+
+    @PostMapping(value = "/testArr")
+    public String testArr(@RequestBody ArrayList<Integer> arrayList) {
+
+        if (arrayList instanceof ArrayList) {
+            log.info("是数组");
+        }
+        log.info(arrayList.toString());
+        return "ok";
     }
 
 
--- test/src/main/java/com/daxue/test/main/Test04.java
+++ test/src/main/java/com/daxue/test/main/Test04.java
@@ -1,5 +1,6 @@
 package com.daxue.test.main;
 
+
 /**
  * @author wangxuedi
  * Date: 2020/5/11 16:13
@@ -9,12 +10,43 @@ public class Test04 {
     public static void main(String[] args) {
         String usr = "15936312258";
 
-        boolean matches = usr.matches("^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(17[013678])|(18[0,5-9]))\\d{8}$");
-        if (matches) {
-            System.out.println("是正确的手机号");
-        }else {
-            System.out.println("错误的手机号");
+//        boolean matches = usr.matches("^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(17[013678])|(18[0,5-9]))\\d{8}$");
+//        if (matches) {
+//            System.out.println("是正确的手机号");
+//        }else {
+//            System.out.println("错误的手机号");
+//        }
+
+//        String str = "0";
+//        Integer i = 0;
+//        boolean equals = "5".equals(5);
+//        if ("5".equals(5)) {
+//            System.out.println("true");
+//        }
+
+
+//        ArrayList<Integer> array = new ArrayList<>();
+//        array.add(1);
+//        array.add(2);
+//        array.add(3);
+//        array.add(4);
+
+        String str = "[65, 67]";
+        String[] split = str.substring(1, str.length() - 1).split(",");
+
+        for (int i =0; i< split.length ;i++) {
+            split[i] = split[i].trim();
         }
+
+        System.out.println("chirden");
+
+        //        for (String s: split) {
+//            String trim = s.trim();
+//        }
+//        System.out.println(array);
+
+
+        int[] a = {1,2,3,4};
 
 
     }
