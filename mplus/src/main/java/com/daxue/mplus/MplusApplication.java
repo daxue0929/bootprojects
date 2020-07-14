2316a18517926809855892a5e78ac50f876710da
wangxuedi <wangxuedi@izton.com>
2020-07-14 16:08:03 +0800

    mpplus

---
+++ mplus/src/main/java/com/daxue/mplus/MplusApplication.java
@@ -0,0 +1,13 @@
+package com.daxue.mplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MplusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MplusApplication.class, args);
+    }
+
+}
---
+++ mplus/src/test/java/com/daxue/mplus/MplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daxue.mplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MplusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
